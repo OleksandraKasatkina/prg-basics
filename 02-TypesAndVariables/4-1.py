262a72e52bbc9ae11a43194b26358c9c7cc56b34
Oleksandra Kasatkina <236292@student.uek.krakow.pl>
2024-10-11 10:16:50 +0200

    Section 4

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,10 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = "7"
+age2  = "13"
+height = "142 "
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height}cm.")
+print(f"In 6 years I will be {age2} years old.")
