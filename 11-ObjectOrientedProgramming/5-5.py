e3c6c7a6f6a40d0cebd68ec67e896eb5819b653d
Oleksandra Kasatkina <236292@student.uek.krakow.pl>
2024-12-26 18:27:08 +0100

    upd

--- 11-ObjectOrientedProgramming/5-5.py
+++ 11-ObjectOrientedProgramming/5-5.py
@@ -1,23 +1,17 @@
-# main.py file
-from thermometer import Thermometer  # Import the Thermometer class from thermometer.py
+from thermometer import Thermometer
 
 def main():
-    # Create a thermometer object
     my_thermometer = Thermometer()
 
-    # Turn the thermometer on
     print("Turning the thermometer on...")
     my_thermometer.turn_on()
 
-    # Measure the temperature
     print("Measuring temperature...")
     my_thermometer.measure_temperature()
 
-    # Display the temperature
     print("Displaying temperature...")
     my_thermometer.display_temperature()
 
-    # Turn the thermometer off
     print("Turning the thermometer off...")
     my_thermometer.turn_off()
 
